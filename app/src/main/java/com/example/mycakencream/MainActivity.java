961bdb34aa73ae56fe35213adf326b00868c1dca
Motasim Foad <motasimfoad@gmail.com>
2021-08-14 17:16:45 -0400

    Added button on macin activity

--- app/src/main/java/com/example/mycakencream/MainActivity.java
+++ app/src/main/java/com/example/mycakencream/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.mycakencream;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_auth = (Button) findViewById(R.id.btn_auth);
+        btn_auth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authScreen();
+            }
+        });
+    }
+
+    public void authScreen(){
+        Intent intent = new Intent(MainActivity.this, Auth.class);
+        startActivity(intent);
     }
 }
